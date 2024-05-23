5ecff82e5b5eae5dce29ba5f7cf413a695ed1f1a
byalexandar <alexandermeyerun@gmail.com>
2024-05-23 02:24:02 +0200

    Big Update All Info

--- sidebars.js
+++ sidebars.js
@@ -611,11 +611,11 @@ const sidebars = {
           type: "category",
           label: "BF tokens",
           items: [
-            "developer-docs/defi/icp-tokens/overview",
-            "developer-docs/defi/icp-tokens/ledger-local-setup",
-            "developer-docs/defi/icp-tokens/using-the-ledger",
-            "developer-docs/defi/icp-tokens/icp-index-local-setup",
-            "developer-docs/defi/icp-tokens/account-trimming",
+            "developer-docs/defi/bf-tokens/overview",
+            "developer-docs/defi/bf-tokens/ledger-local-setup",
+            "developer-docs/defi/bf-tokens/using-the-ledger",
+            "developer-docs/defi/bf-tokens/icp-index-local-setup",
+            "developer-docs/defi/bf-tokens/account-trimming",
           ],
         },
         {
