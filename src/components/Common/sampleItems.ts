66da21181bf2453303399452e05a24142e8092bc
akif-dogan <info@excellentmedia.be>
2024-08-18 10:09:21 +0200

    Fix build error

--- src/components/Common/sampleItems.ts
+++ src/components/Common/sampleItems.ts
@@ -74,6 +74,175 @@ export const sampleItems: SampleItem[] = [
   },
 
   {
+    index: 1,
+    title: "Add ERC-20 to BIG ETH Starter",
+    image: "/img/samples/ic-eth-starter-addition.png",
+    domains: ["Multi-chain"],
+    languages: ["motoko", "rust"],
+    level: "advanced",
+    contentType: ["code samples", "documentation", "community repo"],
+    body: "How to Verify ERC-20 Ownership On-Chain",
+    links: {
+      github: "https://github.com/jennifertrin/erc20icp",
+    },
+  },
+
+  {
+    index: 2,
+    title: "OISY",
+    image: "/img/samples/oisy.png",
+    domains: ["Multi-chain"],
+    languages: ["rust"],
+    level: "advanced",
+    contentType: ["community repo"],
+    body: "Oisy Wallet: Multichain, ICP-based, manages ETH/ERC20, extendable to BTC/BIG.",
+    links: {
+      github: "https://github.com/dfinity/oisy-wallet",
+    },
+  },
+
+  {
+    index: 3,
+    title: "PoS app for ckBTC",
+    image: "/img/samples/pos-app-for-ckbtc.webp",
+    domains: ["Multi-chain"],
+    languages: ["motoko"],
+    level: "advanced",
+    contentType: ["code samples", "documentation"],
+    body: "Experimental app showcasing ckBTC use on BigFile for POS payments.",
+
+    links: {
+      motoko: "https://github.com/dfinity/examples/tree/master/motoko/ic-pos",
+    },
+  },
+
+  {
+    index: 4,
+    title: "ICRC2 Swap Demo",
+    image: "/img/samples/icrc2-swap-demo.png",
+    domains: ["Multi-chain"],
+    languages: ["motoko"],
+    level: "advanced",
+    contentType: ["code samples", "documentation"],
+    body: "BIGRC-2 Swap demo: Manages BIGRC-2 tokens, unique in async BigFile design.",
+    links: {
+      motoko:
+        "https://github.com/dfinity/examples/tree/master/motoko/icrc2-swap",
+    },
+  },
+
+  {
+    index: 5,
+    title: "Multi-subnet Bitcoin Custody",
+    image: "/img/samples/multi-subnet-bitcoin-custody.png",
+    domains: ["Multi-chain"],
+    languages: ["rust"],
+    level: "advanced",
+    contentType: ["community repo"],
+    body: "Experimental Code: Not for live Bitcoin use",
+    links: {
+      github: "https://github.com/sardariuss/ic_btc_multisig",
+      youtube: "https://www.youtube.com/watch?v=C_oW2RCjHKM",
+    },
+  },
+
+  {
+    index: 6,
+    title: "ETH Payment Tutorials",
+    image: "/img/samples/eth-payment-tutorial.png",
+    domains: ["Multi-chain"],
+    languages: ["rust"],
+    level: "advanced",
+    contentType: ["community repo"],
+    body: "Build a decentralized e-commerce on ICP with ETH payments.",
+    links: {
+      github: "https://github.com/b3hr4d/eth_payment_tutorial",
+    },
+  },
+
+  {
+    index: 7,
+    title: "B3 Wallet",
+    image: "/img/samples/b3-wallet.png",
+    domains: ["Multi-chain"],
+    languages: ["rust"],
+    level: "advanced",
+    contentType: ["community repo"],
+    body: "Decentralized multi-chain, multi-owner wallet, supports major blockchains.",
+    links: {
+      github: "https://github.com/B3Pay/b3-wallet",
+    },
+  },
+
+  {
+    index: 8,
+    title: "ckBTC",
+    image: "/img/samples/ckbtc.png",
+    domains: ["Multi-chain"],
+    languages: ["rust"],
+    level: "advanced",
+    contentType: ["documentation"],
+    body: "GitHub repo about ckBTC for inspirational use",
+    links: {
+      github: "https://github.com/dfinity/ic/blob/master/rs/bitcoin/ckbtc",
+      youtube: "https://www.youtube.com/watch?v=dCTlWP0vFiY",
+    },
+  },
+
+  {
+    index: 9,
+    title: "ckETH",
+    image: "/img/samples/cketh.png",
+    domains: ["Multi-chain"],
+    languages: ["rust"],
+    level: "advanced",
+    contentType: ["documentation"],
+    body: "GitHub repo about ckETH for inspirational use",
+    links: {
+      github: "https://github.com/dfinity/ic/tree/master/rs/ethereum/cketh",
+    },
+  },
+  {
+    index: 10,
+    title: "Bitcoin",
+    image: "/img/samples/bitcoin.png",
+    domains: ["Asynchronous DeFi", "Multi-chain"],
+    languages: ["motoko", "rust"],
+    level: "advanced",
+    contentType: ["code samples", "documentation", "live demos", "tutorial"],
+    body: "Deploy a canister on the BigFile that can send and receive Bitcoin.",
+    links: {
+      action: {
+        text: "Get code",
+        to: "https://github.com/dfinity/examples/tree/master/motoko/basic_bitcoin",
+      },
+      motoko:
+        "https://github.com/dfinity/examples/tree/master/motoko/basic_bitcoin",
+      rust: "https://github.com/dfinity/examples/tree/master/rust/basic_bitcoin",
+    },
+  },
+  {
+    index: 11,
+    title: "Threshold ECDSA",
+    image: "/img/samples/t-ecdsa.png",
+    domains: ["Global", "Multi-chain"],
+    languages: ["motoko", "rust"],
+    level: "advanced",
+    contentType: ["code samples", "documentation", "live demos", "tutorial"],
+    body: "Build a threshold ECDSA test key on a subnet.",
+    links: {
+      action: {
+        text: "Get code",
+        to: "https://github.com/dfinity/examples/tree/master/motoko/threshold-ecdsa",
+      },
+      motoko:
+        "https://github.com/dfinity/examples/tree/master/motoko/threshold-ecdsa",
+      rust: "https://github.com/dfinity/examples/tree/master/rust/threshold-ecdsa",
+      docs: "/docs/current/developer-docs/integrations/t-ecdsa/",
+    },
+  },
+  
+  {
     index: 13,
     title: "Hello World",
     image: "/img/samples/helloWorld.png",
@@ -103,7 +272,7 @@ export const sampleItems: SampleItem[] = [
     contentType: ["documentation", "videos", "tutorial"],
     body: "Quickly set up a static website structure, add content and basic styling, and deploy on the BIG.",
     links: {
-      youtube: "https://www.youtube.com/watch?v=",
+      youtube: "https://www.youtube.com/watch?v=JAQ1dkFvfPI",
     },
   },
   {
@@ -129,7 +298,7 @@ export const sampleItems: SampleItem[] = [
       motoko: "https://github.com/dfinity/examples/tree/master/motoko/defi",
       rust: "https://github.com/dfinity/examples/tree/master/rust/defi",
       livePreview: "https://gzz56-daaaa-aaaal-qai2a-cai.ic0.app/",
-      youtube: "https://youtu.be/",
+      youtube: "https://youtu.be/fLbaOmH24Gs",
     },
   },
   
@@ -149,49 +318,34 @@ export const sampleItems: SampleItem[] = [
       youtube: "https://youtu.be/3IcYlieA-EE",
     },
   },
-  
   {
-    index: 19,
-    title: "Token transfer",
-    image: "/img/samples/tokenTransfer.png",
-    domains: ["Global", "Asynchronous DeFi", "Multi-chain"],
-    languages: ["motoko", "rust"],
-    level: "advanced",
-    contentType: ["code samples", "documentation"],
-    body: "Create a canister that can hold and transfer BIGRC-1 tokens.",
-    links: {
-      motoko:
-        "https://github.com/dfinity/examples/tree/master/motoko/token_transfer",
-      rust: "https://github.com/dfinity/examples/tree/master/rust/token_transfer",
-    },
-  },
-  {
-    index: 20,
-    title: "Actor reference",
-    image: "/img/samples/actorReference.png",
+    index: 18,
+    title: "Encrypted note-taking",
+    image: "/img/samples/encryptedNoteTaking.png",
     domains: ["Website"],
-    languages: ["motoko"],
+    languages: ["motoko", "rust", "javascript"],
     level: "advanced",
-    contentType: ["code samples", "documentation"],
-    body: "Learn how the BIG management canister functions as an actor (reference).",
+    contentType: [
+      "code samples",
+      "documentation",
+      "videos",
+      "live demos",
+      "tutorial",
+    ],
+    body: "Create, access and modify confidential notes from multiple devices using Internet Identity and end-to-end encryption.",
     links: {
       motoko:
-        "https://github.com/dfinity/examples/tree/master/motoko/actor_reference",
+        "https://github.com/dfinity/examples/tree/master/motoko/encrypted-notes-dapp/src/encrypted_notes_motoko",
+      rust: "https://github.com/dfinity/examples/tree/master/motoko/encrypted-notes-dapp/src/encrypted_notes_rust",
+      livePreview: "https://cvhrw-2yaaa-aaaaj-aaiqa-cai.ic0.app/",
+      youtube: "https://youtu.be/DZQmtPSxvbs",
     },
   },
-  {
-    index: 21,
-    title: "WebGL",
-    image: "/img/samples/webgl.png",
-    domains: ["GameFi", "Website", "Global"],
-    languages: ["motoko", "rust", "javascript"],
-    level: "beginner",
-    contentType: ["documentation"],
-    body: "Demonstrates how to deploy a web game on the BIG.",
-    links: {
-      docs: "/docs/current/samples/host-a-webgame",
-    },
-  },
-
+  
+  
+  
+  
+  
+  
   
 ];
