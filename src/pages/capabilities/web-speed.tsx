9e1262ee2fec2db6c085e97150909821571a3fd8
akif-dogan <info@excellentmedia.be>
2024-08-30 23:25:06 +0200

    Edit web-speed pagina edit

--- src/pages/capabilities/web-speed.tsx
+++ src/pages/capabilities/web-speed.tsx
@@ -20,15 +20,9 @@ function FeaturePage() {
             <div className="md:w-7/10">
               <h1 className="tw-heading-3 md:tw-heading-2 mb-6">Web speed</h1>
               <p className="tw-lead-sm md:tw-lead mb-0">
-                Users can interact with dapps on the BigFile (BIG) with
-                speeds comparable to traditional web applications. Developers
-                can therefore build a much broader range of fully on-chain dapps
-                without relying on cloud services and risking single points of
-                failure.
+              Users can interact with dapps on the BigFile (BIG) with speeds comparable to traditional web applications. Developers can therefore build a much broader range of fully on-chain dapps without relying on cloud services and risking single points of failure.
                 <br />
-                <br />
-                Concretely, query (read) calls are answered in ~100
-                milliseconds, and update (write) calls in ~2 seconds.
+                
               </p>
             </div>
           </div>
@@ -66,45 +60,21 @@ function FeaturePage() {
 
             <h3>Read vs Write calls</h3>
             <p>
-              Common practice in computer science and databases is to separate
-              tasks into read tasks and write tasks. As the former are less
-              'expensive' in terms of time, categorizing types of calls can
-              substantially help to increase the efficiency of a program. As a
-              blockchain partly acts as a distributed database, it is useful to
-              consider this separation of tasks in this case too. Following this
-              line of thinking, the BigFile facilitates two type of
-              transactions, query calls (read-only) and update (write) calls.
+            In computer science and databases, it is common practice to separate tasks into read and write operations. Since read tasks are generally less 'expensive' in terms of time, categorizing these operations can significantly enhance a program's efficiency. Given that a blockchain functions in part as a distributed database, this separation of tasks is also valuable here. In line with this approach, BigFile supports two types of transactions: query calls (read-only) and update (write) calls.
               <br />
               <br />
-              As of May 24, 2022, The BigFile can handle more than 1
-              million queries per second and more than 20 thousand update calls
-              per second. This is shown in{" "}
-              <Link href="https://wiki.thebigfile.com/wiki/Internet_Computer_performance_%26_power_consumption">
-                performance tests
-              </Link>{" "}
+              
             </p>
             <h3>Chain Key Cryptography</h3>
             <p>
-              The main advantage of using a blockchain or decentralized protocol
-              is that the central point of trust is removed. This advantage
-              comes at a cost; trust is not removed, it's decentralized which
-              means that multiple people (or machines or nodes) need to agree on
-              the 'truth' at a given point in time. For multiple people/nodes to
-              agree, they each sign their version of the truth, and each verify
-              all of the others' signatures.
+            The main advantage of using a blockchain or decentralized protocol is that the central point of trust is removed. This advantage comes at a cost; trust is not removed, it's decentralized which means that multiple people (or machines or nodes) need to agree on the 'truth' at a given point in time. For multiple people/nodes to agree, they each sign their version of the truth, and each verify all of the others' signatures.
               <br />
               <br />
-              In most blockchain protocols, this is the large source of
-              inefficiency as verifying many individual signatures can be slow.
-              One of the key innovations of the BIG is the introduction of{" "}
-              <Link href="https://medium.com/dfinity/chain-key-technology-one-public-key-for-the-internet-computer-6a3644901e28">
+              
+              In most blockchain protocols, a significant source of inefficiency is the slow process of verifying numerous individual signatures. One of the key innovations of BIG is the introduction of {" "}
+              <Link href="https://forum.thebigfile.com/t/chain-key-cryptography-bigfile/32">
                 Chain Key (CK) Cryptography
-              </Link>{" "}
-              which allows BIG signatures to be verified with a single public
-              key, hence reducing friction for all types of devices (node
-              machines, laptops, mobile phones) that want to efficiently
-              interact with a blockchain with similar speed and security of
-              Web2, but without the centralization.
+              </Link>{" "}, which enables BIG signatures to be verified with a single public key. This reduces friction for all types of devices—whether node machines, laptops, or mobile phones—allowing them to interact with the blockchain as efficiently and securely as Web2, but without the centralization.  
             </p>
 
             <p className="mb-3 mt-6">
@@ -136,13 +106,8 @@ function FeaturePage() {
             />
             <CardWithDescription
               title="Build dapps with the language of your choice"
-              description="Learch about programming languages on the ICP."
+              description="Learch about programming languages on the BigFile."
               href="/docs/current/developer-docs/smart-contracts/write/overview"
-            />
-            <CardWithDescription
-              title="Community Conversation"
-              description="Learn about performance and scalabilty of the ICP."
-              href="https://www.youtube.com/watch?v=XvE0ZsAYKiM"
             />
             <CardWithDescription
               title="BIG Wiki"
